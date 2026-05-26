2c3b0654f0e4840311f8e51e7b466d796839f5cf
pdane <parsunic@gmail.com>
2026-05-26 16:29:54 -0400

    Auto-deploy: update dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './styles/globals.css'
+import { doRollover, injectRecurringTasks } from './lib/init.js'
+
+doRollover()
+injectRecurringTasks()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
