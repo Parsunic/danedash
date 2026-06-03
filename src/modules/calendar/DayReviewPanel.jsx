d829e1aa8dfbde0046a3eb6b9de2356d2372d95e
pdane <parsunic@gmail.com>
2026-06-03 15:04:42 -0400

    Auto-deploy: update dashboard

--- src/modules/calendar/DayReviewPanel.jsx
+++ src/modules/calendar/DayReviewPanel.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef, useCallback } from 'react'
+import { createPortal } from 'react-dom'
 import { supabase } from '../../lib/supabase.js'
 import { getDayEvents } from './calendarUtils.js'
 
