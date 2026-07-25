697acb563c08ad2f18b80bf31fc7fdcb595ec5b1
Parsunic <62620332+Parsunic@users.noreply.github.com>
2026-07-24 20:28:04 -0400

    Auto-deploy: update dashboard

--- src/modules/todo/Todo.jsx
+++ src/modules/todo/Todo.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef, useCallback } from 'react'
 import { storeGet, storeSet, storeDelete, storeListKeys } from '../../lib/storage.js'
+import { writeFresh, reorderFresh } from './taskListOps.js'
 import { isAudioEnabled, playDing } from '../../lib/audio.js'
 import { getActiveDateString, getTomorrowDateString, formatDate } from '../../lib/dateHelpers.js'
 import BackgroundBlob from '../../components/BackgroundBlob.jsx'
